7a5e558b807daedaf0fdde02169338a72044d9af
Гунарді Сутанто <lonecalvary78.de@gmail.com>
2024-10-11 14:39:13 +0800

    [Fix]: Fixing the checkstyle problem

--- src/main/java/lonecalavary78/util/data/DataPartitionerUtil.java
+++ src/main/java/lonecalavary78/util/data/DataPartitionerUtil.java
@@ -1,20 +1,21 @@
 package lonecalavary78.util.data;
 
-import lonecalavary78.util.data.model.Partitioner;
-
 import java.util.ArrayList;
 import java.util.List;
+import lonecalavary78.util.data.model.Partitioner;
 
-public class DataPartitionerUtil<T extends Object> {
+
+public class DataPartitionerUtil<T> {
 
   public List<Partitioner<T>> splitIntoPartitioner(List<T> dataSet) {
     var partitioners = new ArrayList<Partitioner<T>>();
-    if(isDataSetEmpty(dataSet))
+    if (isDataSetEmpty(dataSet)) {
       throw new IllegalArgumentException("The empty source data is required to perform this operation");
+    }
     var maximumRecordPerChunk = getMaximumRecordPerChunk(dataSet.size());
     var totalChunks = Math.ceilDiv(dataSet.size(), maximumRecordPerChunk);
     var counter = 0;
-    while(counter<totalChunks) {
+    while (counter < totalChunks) {
       partitioners.add(new Partitioner<>(counter, dataSet.subList(startPos(counter, maximumRecordPerChunk), endPos(counter, maximumRecordPerChunk, dataSet.size()))));
       counter++;
     }
@@ -26,23 +27,28 @@ public class DataPartitionerUtil<T extends Object> {
   }
 
   private int getMaximumRecordPerChunk(int totalRecords) {
-    var baseNumber=10;
-    if(totalRecords<=0)
+    var baseNumber = 10;
+    if (totalRecords <= 0) {
       throw new IllegalArgumentException("The total record with 0 or less is not allow to get calculated divider");
+    }
+
     var exponent = Math.ceil(Math.log10(totalRecords));
-    if(exponent<=2 && exponent>1)
-      exponent-=1;
-    else if(exponent>2)
-      exponent-=2;
-    return (int)Math.pow(baseNumber, exponent);
+    if (exponent <= 2 && exponent > 1) {
+      exponent -= 1;
+    } else if (exponent > 2) {
+      exponent -= 2;
+    }
+    return (int) Math.pow(baseNumber, exponent);
   }
 
   private int startPos(int chunkCounter, int maximumRecordPerChunk) {
-    return (chunkCounter==0)?0:(chunkCounter-1)*maximumRecordPerChunk;
+    return (chunkCounter == 0) ? 0 : (chunkCounter - 1) * maximumRecordPerChunk;
   }
 
+  @SuppressWarnings("checkstyle:ParenPad")
   private int endPos(int chunckCounter, int maximumRecordPerChunk, int totalRecords) {
-    var end = (chunckCounter<=0)?maximumRecordPerChunk:chunckCounter*maximumRecordPerChunk;
-    return (end>totalRecords)?(totalRecords):end;
+    var end = (chunckCounter <= 0 ) ? maximumRecordPerChunk
+            : chunckCounter * maximumRecordPerChunk;
+    return Math.min(end, totalRecords);
   }
 }
